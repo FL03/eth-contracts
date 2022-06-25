be59fb3317a4a3444aba642b81cae2efc64fdef5
FL03 <jo3mccain@icloud.com>
2022-06-25 02:03:58 -0500

    Update

--- src/contracts/core/interfaces/IERC20.sol
+++ src/contracts/core/interfaces/IERC20.sol
@@ -2,5 +2,11 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract IERC20 {
+    uint8 decimals;
+    string appellation;
+    string symbol;
 
+    function name() public view returns (memory string) {
+        return appellation;
+    }
 }
--- src/contracts/data/Primitives.sol
+++ src/contracts/data/Primitives.sol
@@ -2,9 +2,25 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 library Structs {
+    struct Appellation {
+        string name;
+        string slug;
+    }
    
     struct KeyValue {
         string key;
         string value;
+    }
+
+    struct Context {
+        address actor;
+        mapping(string => string) configuration;
+    }
+
+    struct DataContainer {
+        address actor;
+        address client;
+
+        mapping(string => mapping(uint => string[])) data;
     }
 }
